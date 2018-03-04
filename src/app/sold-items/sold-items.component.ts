284baac7df69dffba4e079a8b33cabbfb58232d6
aldyson <adrian_dyson_1996@hotmail.co.uk>
2018-03-04 16:40:14 +0000

    Page headings updated.

---
+++ src/app/sold-items/sold-items.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-sold-items',
+  templateUrl: './sold-items.component.html',
+  styleUrls: ['./sold-items.component.css']
+})
+export class SoldItemsComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
